fdef04a9ab1ebc2a1d82b31bcb6122035f190411
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-10 08:27:10 -0500

    fetching images from unsplash

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form action="" className="ui form">
-          <div className='field'>
-          <label htmlFor="">Image Search</label>
-            <input type="text" name="" id="" />
+        <form action="" className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label htmlFor="">Image Search</label>
+            <input
+              type="text"
+              name=""
+              value={this.state.term}
+              onChange={(e) =>
+                console.log(this.setState({ term: e.target.value }))
+              }
+            />
           </div>
         </form>
       </div>
