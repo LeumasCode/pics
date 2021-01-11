aaa27a7c6ace6d6610de1006412ee5e92260f411
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-11 05:51:40 -0500

    getting the image height

---
+++ src/components/ImageCard.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    return console.log(this.imageRef.current.clientHeight);
+  };
+
+  render() {
+    const { urls, description } = this.props.image;
+
+    return (
+      <div>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
